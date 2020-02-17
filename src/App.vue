90766a8b1f882383eaf894bf5f053423fc05aaaf
trrtly <328602875@qq.com>
2020-02-18 00:03:17 +0800

    perf: format

--- src/App.vue
+++ src/App.vue
@@ -45,12 +45,7 @@ export default {
         localStorage.setItem('token', res.data.token)
         localStorage.setItem('expiresAt', res.data.expiresAt)
         this.defaultProcess()
-      } 
-      // else {
-      //   this.$toast('暂未开启微信验证，请手动填写code')
-      // }
-      // 微信验证，测试环境先关闭
-      else {
+      } else { // 微信验证，测试环境先关闭
         let platform = JSON.parse(localStorage.getItem('platform'))
         let redirectUri = encodeURIComponent(window.location.href)
         let url = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${platform.appid}&redirect_uri=${redirectUri}&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect`
